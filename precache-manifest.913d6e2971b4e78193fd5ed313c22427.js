69238c644dac88fdd8dc673a1d1d8150dc28f197
mac <mac@macs-MacBook-Pro.local>
2020-09-30 17:51:20 +0200

    Updates

---
+++ precache-manifest.913d6e2971b4e78193fd5ed313c22427.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220776937a4c9e6e365eb937b3b0715b",
+    "url": "/fancyRobots/index.html"
+  },
+  {
+    "revision": "8c95af2c9c44f2206132",
+    "url": "/fancyRobots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f059c9587d2b1e0d169b",
+    "url": "/fancyRobots/static/css/main.b24442db.chunk.css"
+  },
+  {
+    "revision": "8c95af2c9c44f2206132",
+    "url": "/fancyRobots/static/js/2.7dc1175e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fancyRobots/static/js/2.7dc1175e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f059c9587d2b1e0d169b",
+    "url": "/fancyRobots/static/js/main.0f105d51.chunk.js"
+  },
+  {
+    "revision": "7a00a2a8bc9444019bf4",
+    "url": "/fancyRobots/static/js/runtime-main.9bff62bd.js"
+  },
+  {
+    "revision": "b6e9bb37f4e5cd1059e6ebb846a6d40e",
+    "url": "/fancyRobots/static/media/SEGA LOGO FONT.b6e9bb37.woff"
+  }
+]);
